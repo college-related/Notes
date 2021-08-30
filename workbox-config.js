b031009109f399f81127499c6fdccbc1e0f581a3
Alson33 <garbuja12alson@gmial.com>
2021-08-30 15:53:55 +0545

    updated workbox-config for SW

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,28 @@
 module.exports = {
 	globDirectory: 'DSA-Test/',
 	globPatterns: [
-		'**/*.{png,ico,jpg,JPG,gif,css,html,js}'
+		'*/*.{png,ico,jpg,JPG,gif,css,html,js}'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
-	swDest: 'DSA-Test/js/sw.js'
+	swDest: 'DSA-Test/js/sw.js',
+	runtimeCaching: [{
+		// Match any request that ends with .png, .jpg, .jpeg or .svg.
+		urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+	
+		// Apply a cache-first strategy.
+		handler: 'CacheFirst',
+	
+		options: {
+		  // Use a custom cache name.
+		  cacheName: 'images',
+	
+		  // Only cache 10 images.
+		  expiration: {
+			maxEntries: 10,
+		  },
+		},
+	  }],
 };
